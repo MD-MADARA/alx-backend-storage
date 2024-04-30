926758ea74b1b9e933f089a7140e8feb386af331
MD-MADARA <mouadotaku9421@gmail.com>
2024-04-30 12:49:02 +0100

    task1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,5 +3,5 @@
 CREATE TABLE IF NOT EXISTS `users` (
     `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(100) 
+    `name` VARCHAR(255) 
 );
