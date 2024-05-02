4b5c424b8181ab7f7710202f91d8c884d12af19d
MD-MADARA <mouadotaku9421@gmail.com>
2024-05-02 17:05:23 +0100

    fix

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,8 +6,9 @@ AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email != NEW.email
-    THEN UPDATE users
-    SET NEW.valid_email = 0
+        THEN UPDATE users
+        SET NEW.valid_email = 0;
+    END IF;
 END;
 //
 DELIMITER ;
