2882a6a9aa9d13013f22f336dfa182b8e6fd0799
MD-MADARA <mouadotaku9421@gmail.com>
2024-05-02 16:59:45 +0100

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
+
+DELIMITER //
+CREATE TRIGGER email_validation
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    UPDATE users
+    SET valid_email = 0
+    WHERE OLD.email != NEW.email
+END;
+//
+DELIMITER ;
