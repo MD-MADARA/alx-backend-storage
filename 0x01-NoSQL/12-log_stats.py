5a802944d19b318c8d39a98fb35b1a5aa8fe903c
MD-MADARA <mouadotaku9421@gmail.com>
2024-05-06 14:45:20 +0100

    clean

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,24 +4,15 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs.nginx
+    collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
 
-    num_logs = db.count_documents({})
-    print(f"{num_logs} logs")
+    logs = collection.count_documents({})
+    print(f"{logs} logs\nMethods")
 
-    get = db.count_documents({'method': 'GET'})
-    post = db.count_documents({'method': 'POST'})
-    put = db.count_documents({'method': 'PUT'})
-    patch = db.count_documents({'method': 'PATCH'})
-    delete = db.count_documents({'method': 'DELETE'})
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for i in range(len(methods)):
+        n = collection.count_documents({"method": methods[i]})
+        print(f"\tmethod {methods[i]}: {n}")
 
-    print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-
-    status = db.count_documents({'method': 'GET', 'path': '/status'})
+    status = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status} status check")
